d74aa23b5c01907089e94b947a90a6497709a3f0
t2lights <136258184+t2lights@users.noreply.github.com>
2023-12-17 16:31:55 -0500

    again

--- plugin_setup.php
+++ plugin_setup.php
@@ -3,12 +3,12 @@
 $pluginName = basename(dirname(__FILE__));
 $logFile = $settings['logDirectory']."/".$pluginName.".log";
 $myPid = getmypid();
-$settingsFile = $settings['configDirectory']."/"."plugin.".$pluginName.".ini";
+// $settingsFile = $settings['configDirectory']."/"."plugin.".$pluginName.".ini";
 
 echo "Plugin Name:  ".$pluginName."<br/> \n";
 echo "Logfile:      ".$logfile."<br/> \n";
 echo "My PID:       ".$myPid."<br/> \n";
-echo Settings File: ".$settingsFile.",br/> \n";
+echo "Settings File: ".$settingsFile.",br/> \n";
 
 ?>
 
