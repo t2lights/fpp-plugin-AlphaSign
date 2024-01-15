de7b636bf907f8fd39f490b823bb7fba8d1044ba
t2lights <136258184+t2lights@users.noreply.github.com>
2024-01-14 21:18:12 -0500

    One more time

--- functions.php
+++ functions.php
@@ -2,15 +2,10 @@
 
 function logEntry($data) {
 	global $logFile,$myPid;
-	echo $logFile." ".$myPid;
 	$data = $_SERVER['PHP_SELF']." : [".$myPid."] ".$data;
-	echo $data;
 	$logWrite= fopen($logFile, "a") or die("Unable to open file!");
-	echo "B";
 	fwrite($logWrite, date('Y-m-d h:i:s A',time()).": ".$data."\n");
-	echo "C";
 	fclose($logWrite);
-	echo "D";
 }
 
 ?>
