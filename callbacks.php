6ffcab02b4b1e6d0488330fe1d99e130e0b89411
t2lights <136258184+t2lights@users.noreply.github.com>
2024-01-14 22:35:22 -0500

    One more

---
+++ callbacks.php
@@ -0,0 +1,6 @@
+<?php
+
+logEntry("callbacks.php is being run");
+
+?>
+
--- plugin_setup.php
+++ plugin_setup.php
@@ -3,6 +3,7 @@
 error_reporting(E_ALL);
 include_once "functions.php";
 
+$debug=true;
 $pluginName = basename(dirname(__FILE__));
 $logFile = $settings['logDirectory']."/".$pluginName.".log";
 $myPid = getmypid();
@@ -13,8 +14,6 @@ echo "Logfile:      ".$logFile."<br/> \n";
 echo "My PID:       ".$myPid."<br/> \n";
 echo "Settings File: ".$settingsFile."<br/> \n";
 
-echo "attempting to log entry"."<br/> \n";
 logEntry("The settings file name is called: ".$settingsFile);
-echo "did the log entry work"."<br/> \n";
 ?>
 
