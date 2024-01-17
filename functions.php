75ed9ee1990a03a0b52a492cd8b962f5aeef7c68
t2lights <136258184+t2lights@users.noreply.github.com>
2024-01-16 22:32:58 -0500

    sfdfg

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+function logEntry($data) {
+	global $logFile,$myPid;
+	$data = $_SERVER['PHP_SELF']." : [".$myPid."] ".$data;
+	$logWrite= fopen($logFile, "a") or die("Unable to open file!");
+	fwrite($logWrite, date('Y-m-d h:i:s A',time()).": ".$data."\n");
+	fclose($logWrite);
+}
+
+?>
+
